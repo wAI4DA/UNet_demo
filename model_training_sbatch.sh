16fa0b511690cb22c3a11b2bee8e29bce4e00fc7
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-11-18 19:26:12 -0700

    update model_training_sbatch.sh and .gitignore

--- model_training_sbatch.sh
+++ model_training_sbatch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -A gpu-wizard #Replace with whatever your GPU project is, if not gpu-wizard
-#SBATCH -t 2:00:00 #Change if more time is needed
+#SBATCH -t 4:00:00 #Change if more time is needed
 #SBATCH -J UNet_training
 #SBATCH -o JOB_LOG_%x_%J.out
 #SBATCH -p ursa
@@ -12,12 +12,11 @@
 #SBATCH --partition=u1-h100
 #SBATCH --gres=gpu:h100:2
 
-#SBATCH --exclude=u22g03 #This node has a failing GPU and shouldn't be used
-
 export OMP_NUM_THREADS=24 #must match --cpus-per-task
 nvidia-smi
-echo 'Python loaded'
+echo "start at $(date)"
 
 source /scratch3/BMC/wrfruc/gge/AI/ai4da/load_ai4da.sh 
 srun python3 -u model_training.py
 
+ech "finish at $(date)"
